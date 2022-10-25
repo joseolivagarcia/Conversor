0425e1773246ce4561b622c2c61c8a0c7d2d30d7
Jose Oliva Local <jose@joseoliva.com>
2022-10-25 14:36:01 +0200

    funcionando la conversor activity a falta de las funciones con su factor de conversion

--- app/src/main/java/joseoliva/com/conversor/ConversorActivity.kt
+++ app/src/main/java/joseoliva/com/conversor/ConversorActivity.kt
@@ -1,14 +1,16 @@
 package joseoliva.com.conversor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import joseoliva.com.conversor.databinding.ActivityConversorBinding
 
 class ConversorActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityConversorBinding
-    var numarray = 0 //necesito esta var para indicarle que array de unidades usare en el spinner
+    var numarray: Int = 0 //necesito esta var para indicarle que array de unidades usare en el spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +27,39 @@ class ConversorActivity : AppCompatActivity() {
         when(medidarecibida){
             "Peso" -> {
                 numarray = R.array.arrayPeso
-                return
+            }
+            "Temperatura" -> {
+                numarray = R.array.arrayTemperatura
+            }
+            "Capacidad HDD" -> {
+                numarray = R.array.arrayHdd
             }
         }
-        //relleno los spinners
 
+        //relleno los spinners
+        val adapspinner = ArrayAdapter.createFromResource(this, numarray, android.R.layout.simple_spinner_item)
+        adapspinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.sporigen.adapter = adapspinner
+        binding.spdestino.adapter = adapspinner
+
+        /*
+        doy funcionalidad al boton de convertir. Segun la medida que hubiese seleccionado
+        ire a una funcion u otra para convertir.
+         */
+        binding.btnconversion.setOnClickListener {
+
+            when(medidarecibida){
+                "Peso" -> {
+                    Toast.makeText(this,"voy a convertir $medidarecibida",Toast.LENGTH_SHORT).show()
+                }
+                "Temperatura" -> {
+                    Toast.makeText(this,"voy a convertir $medidarecibida",Toast.LENGTH_SHORT).show()
+                }
+                "Capacidad HDD" -> {
+                    Toast.makeText(this,"voy a convertir $medidarecibida",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     }
 }
